155249af2476c4c75a0d4502906fe373a23ef5da
ariabee <abrown72@uvm.edu>
2020-05-18 09:06:45 -0400

    adjusted gradle configuration, added initial Planets enum type

---
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -0,0 +1,9 @@
+package org.launchcode.enumerableplanets.data;
+
+public enum Planets {
+    // list the planets here.
+    // Mercury, Venus, Earth, Mars, Jupiter,
+    // Saturn, Uranus, Neptune
+    // Don't forget to capitalization convention and enum
+    // syntax to separate value and end the list
+}
