aea3c16d9f7df09e948053d1385cec4873c70ab2
ariabee <abrown72@uvm.edu>
2020-05-18 10:08:13 -0400

    prepared exercises code

--- src/main/java/org/launchcode/enumerableplanets/data/Planets.java
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -6,4 +6,29 @@ public enum Planets {
     // Saturn, Uranus, Neptune
     // Don't forget to capitalization convention and enum
     // syntax to separate value and end the list
+    MERCURY("Mercury", 88),
+    VENUS("Venus", 225),
+    EARTH ("Earth", 365),
+    MARS ("Mars", 687),
+    JUPITER ("Jupiter", 4333),
+    SATURN ("Saturn", 10759),
+    URANUS("Uranus", 30687),
+    NEPTUNE ("Neptune", 60200);
+
+
+    private final String name;
+    private final int yearLength; // number of Earth days on each planet
+
+    Planets(String name, int yearLength) {
+        this.name = name;
+        this.yearLength = yearLength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearLength() {
+        return yearLength;
+    }
 }
